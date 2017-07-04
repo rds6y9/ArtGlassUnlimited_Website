77141e4238e4b597e6e002a56606b6716e18be00
rds6y9 <rds6y9@mst.edu>
2017-07-04 13:18:28 -0500

    Finalized base.html. Front end ready for content.

---
+++ core/static/core/js/navigation.js
@@ -0,0 +1,8 @@
+function showAndHideMenu() {
+    if(document.getElementById("dropdown-nav-menu").style.display == "block") {
+        document.getElementById("dropdown-nav-menu").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-nav-menu").style.display = "block";
+    }
+}
