16d2e7d9f458b87806b68be8bdde77e91d8f570c
rds6y9 <rds6y9@mst.edu>
2017-07-28 22:49:14 -0500

    Created fresh html and css files for the new responsive mobile-first layout

--- home/views.py
+++ home/views.py
@@ -3,10 +3,10 @@ from .models import HomeSlide
 
 def home_page(request):
 	carousel_slides = HomeSlide.objects.all()
-	return render(request, 'home.html', {'carousel_slides': carousel_slides})
+	return render(request, 'home_revised.html', {'carousel_slides': carousel_slides})
 
 def products_page(request):
-	return render(request, 'products.html')
+	return render(request, 'products_revised.html')
 
 def contact_page(request):
-	return render(request, 'contact.html')
+	return render(request, 'contact_revised.html')
