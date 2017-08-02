ed27bcd5608f47cadba4189c4c6f41439c6c15b5
rds6y9 <rds6y9@mst.edu>
2017-08-02 16:49:24 -0500

    Replaced all non mobile-first templates with new ones. Currently in development

--- core/static/core/js/navigation.js
+++ core/static/core/js/navigation.js
@@ -1,8 +1,9 @@
 function showAndHideMenu() {
-    if(document.getElementById("dropdown-nav-menu").style.display == "block") {
-        document.getElementById("dropdown-nav-menu").style.display = "none";
+	menu = document.getElementById("dropdown-nav-menu");
+    if(menu.style.display == "block") {
+        menu.style.display = "none";
     }
     else {
-        document.getElementById("dropdown-nav-menu").style.display = "block";
+        menu.style.display = "block";
     }
 }
